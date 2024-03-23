1f9cce72318edfbea9cbe34c01f1f9cd7004bb28
valek <valekgodovan@gmail.com>
2024-03-24 02:01:01 +0700

    add database

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from dotenv import load_dotenv
 from flask import render_template
 import os
+import psycopg2
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
+conn = psycopg2.connect(DATABASE_URL)
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
