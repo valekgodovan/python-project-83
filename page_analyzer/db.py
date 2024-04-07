57b64aa9bdc4e9cab13e950645c69ea277c9bb90
valek <valekgodovan@gmail.com>
2024-04-07 23:01:18 +0700

    change connection db

--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -6,13 +6,12 @@ def get_db_connection(database_url):
 
 
 def insert_url(url, conn):
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute("""insert into urls (name) values (%s);""", (url,))
+    with conn.cursor() as cur:
+        cur.execute("""insert into urls (name) values (%s);""", (url,))
+
 
 def is_url_in_db(url, conn):
-    with conn:
-        with conn.cursor() as cur:
-            cur.execute("""select name from urls ;""")
-            urls_in_db = cur.fetchall()
+    with conn.cursor() as cur:
+        cur.execute("""select name from urls ;""")
+        urls_in_db = cur.fetchall()
     return (url,) in urls_in_db
