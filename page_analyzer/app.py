d13d47d4a196d607a2bd144bb5b27aefa0b069d9
valek <valekgodovan@gmail.com>
2024-04-08 22:36:25 +0700

    add flash

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, url_for
 from dotenv import load_dotenv
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, flash, url_for, get_flashed_messages
 from . import db
 import os
 import validators
+import datetime
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
@@ -22,29 +23,38 @@ def index():
 
 @app.route('/urls')
 def urls():
+    messages = get_flashed_messages(with_categories=True)
     return render_template(
         'urls.html',
+        messages=messages,
     )
 
 
 @app.route('/urls/<int:id>')
 def urls_id(id):
+    id, name, date = db.get_row_from_id_db(id, conn)
+    messages = get_flashed_messages(with_categories=True)
     return render_template(
         'urls_id.html',
         id=id,
+        name=name,
+        date=date.date(),
+        messages=messages,
     )
 
 
 @app.post('/urls')
 def urls_post():
     url = request.form.get('url')
-    print(db.is_url_in_db(url, conn))
+    if not validators.url(url):
+        flash('Некорректный URL', 'danger')
+        return redirect(url_for('urls'), code=302)
     if validators.url(url) and not db.is_url_in_db(url, conn):
         db.insert_url(url, conn)
-    id = 2
-
-    return redirect(url_for('urls_id', id=id), code=302)
-
+        flash('Страница успешно добавлена', 'success')
+        id, name, date = db.get_row_from_name_db(url, conn)
+        return redirect(url_for('urls_id', id=id), code=302)
+    return redirect('/urls', code=302)
 
 if __name__ == '__main__':
     app.run()
--- page_analyzer/db.py
+++ page_analyzer/db.py
@@ -12,6 +12,26 @@ def insert_url(url, conn):
 
 def is_url_in_db(url, conn):
     with conn.cursor() as cur:
-        cur.execute("""select name from urls ;""")
+        cur.execute("""select name from urls;""")
         urls_in_db = cur.fetchall()
     return (url,) in urls_in_db
+
+
+def get_row_from_name_db(url, conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+        select * from urls
+        where name = (%s);
+        """, (url,))
+        [(id, name, date)] = cur.fetchall()
+        return id, name, date
+
+
+def get_row_from_id_db(id, conn):
+    with conn.cursor() as cur:
+        cur.execute("""
+        select * from urls
+        where id = (%s);
+        """, (id,))
+        [(id, name, date)] = cur.fetchall()
+        return id, name, date
